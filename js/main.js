96e2a704b270fe17494e6d13737792f322eb7229
positive-jinho <red_horse_@naver.com>
2019-03-17 00:09:21 +0900

    import images

--- js/main.js
+++ js/main.js
@@ -99,49 +99,6 @@ const sortColors = colors => {
 };
 
 function init() {
-  const DB = [
-    {
-      imageUrl:
-        "https://images.unsplash.com/photo-1552625547-6ffd9ceee158?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-      title: "Bryan Apen"
-    },
-    {
-      imageUrl:
-        "https://images.unsplash.com/photo-1552601719-20a552044dc4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80",
-      title: "Engjell Gjepali"
-    },
-    {
-      imageUrl:
-        "https://images.unsplash.com/photo-1552598827-ea6f80fcbcfc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80",
-      title: "Aliona Gumeniuk"
-    },
-    {
-      imageUrl:
-        "https://images.unsplash.com/photo-1552571219-d6e38a3f4849?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-      title: "Benjamin DeYoung"
-    },
-    {
-      imageUrl:
-        "https://images.unsplash.com/photo-1552625547-6ffd9ceee158?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-      title: "Bryan Apen"
-    },
-    {
-      imageUrl:
-        "https://images.unsplash.com/photo-1552601719-20a552044dc4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80",
-      title: "Engjell Gjepali"
-    },
-    {
-      imageUrl:
-        "https://images.unsplash.com/photo-1552598827-ea6f80fcbcfc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80",
-      title: "Aliona Gumeniuk"
-    },
-    {
-      imageUrl:
-        "https://images.unsplash.com/photo-1552571219-d6e38a3f4849?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-      title: "Benjamin DeYoung"
-    }
-  ];
-
   let preview = "";
 
   DB.forEach(element => {
