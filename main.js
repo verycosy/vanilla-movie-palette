03756a834d52966fcc4d85cd0c86797209368e67
positive-jinho <red_horse_@naver.com>
2019-03-16 23:46:54 +0900

    Check cross-origin data error

---
+++ main.js
@@ -0,0 +1,170 @@
+const previewContainer = document.getElementById("preview-container");
+
+const rgbToHex = (r, g, b) =>
+  "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+
+function copy(e) {
+  const color = e.target.dataset.color;
+
+  const temp = document.createElement("input");
+  temp.value = color;
+  document.querySelector("body").append(temp);
+
+  temp.select();
+  document.execCommand("copy");
+  temp.remove();
+
+  const layer = document.querySelector(".layer");
+  document.querySelector(".layer__hex").textContent = color;
+  layer.style.backgroundColor = color;
+  layer.style.top = window.scrollY + "px";
+
+  layer.classList.add("show");
+  setTimeout(() => {
+    layer.classList.remove("show");
+  }, 1200);
+}
+
+const getPalette = e => {
+  const image = e.target;
+  const paletteContainer = image.nextElementSibling;
+
+  const colorThief = new ColorThief();
+  const palette = colorThief.getPalette(image, 11);
+
+  palette.forEach(tmp => {
+    const div = document.createElement("div");
+    const hex = rgbToHex(tmp[0], tmp[1], tmp[2]);
+
+    div.className = "preview__palette";
+    div.style.backgroundColor = hex;
+    div.dataset.color = hex;
+    div.addEventListener("click", copy);
+
+    paletteContainer.appendChild(div);
+  });
+};
+
+const io = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    }
+
+    const target = entry.target;
+    const src = target.dataset.src;
+
+    target.src = src;
+
+    observer.unobserve(target);
+  });
+});
+
+const sortColors = colors => {
+  colors.forEach(color => {
+    const r = color[0] / 255;
+    const g = color[1] / 255;
+    const b = color[2] / 255;
+
+    const max = Math.max.apply(Math, [r, g, b]);
+    const min = Math.min.apply(Math, [r, g, b]);
+
+    const chr = max - min;
+    let hue = 0;
+    let sat = 0;
+    const val = max;
+
+    if (val > 0) {
+      sat = chr / val;
+      if (sat > 0) {
+        if (r == max) {
+          hue = 60 * ((g - min - (b - min)) / chr);
+          if (hue < 0) {
+            hue += 360;
+          }
+        } else if (g == max) {
+          hue = 120 + 60 * ((b - min - (r - min)) / chr);
+        } else if (b == max) {
+          hue = 240 + 60 * ((r - min - (g - min)) / chr);
+        }
+      }
+    }
+
+    color.hue = hue;
+    color.sat = sat;
+    color.val = val;
+  });
+
+  return colors.sort((a, b) => a.hue - b.hue);
+};
+
+function init() {
+  const DB = [
+    {
+      imageUrl:
+        "https://images.unsplash.com/photo-1552625547-6ffd9ceee158?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+      title: "Bryan Apen"
+    },
+    {
+      imageUrl:
+        "https://images.unsplash.com/photo-1552601719-20a552044dc4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80",
+      title: "Engjell Gjepali"
+    },
+    {
+      imageUrl:
+        "https://images.unsplash.com/photo-1552598827-ea6f80fcbcfc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80",
+      title: "Aliona Gumeniuk"
+    },
+    {
+      imageUrl:
+        "https://images.unsplash.com/photo-1552571219-d6e38a3f4849?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+      title: "Benjamin DeYoung"
+    },
+    {
+      imageUrl:
+        "https://images.unsplash.com/photo-1552625547-6ffd9ceee158?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+      title: "Bryan Apen"
+    },
+    {
+      imageUrl:
+        "https://images.unsplash.com/photo-1552601719-20a552044dc4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80",
+      title: "Engjell Gjepali"
+    },
+    {
+      imageUrl:
+        "https://images.unsplash.com/photo-1552598827-ea6f80fcbcfc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80",
+      title: "Aliona Gumeniuk"
+    },
+    {
+      imageUrl:
+        "https://images.unsplash.com/photo-1552571219-d6e38a3f4849?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+      title: "Benjamin DeYoung"
+    }
+  ];
+
+  let preview = "";
+
+  DB.forEach(element => {
+    preview += `
+        <div class="preview">
+            <img class="preview__img" data-src="${
+              element.imageUrl
+            }" cross-origin/>
+            <div class="preview__palette-container"></div>
+            <div class="preview__title"><b>${element.title}</div>
+        </div>`;
+  });
+
+  previewContainer.innerHTML = preview;
+
+  const imgList = document.getElementsByClassName("preview__img");
+
+  for (let e of imgList) {
+    e.addEventListener("load", getPalette);
+    io.observe(e);
+  }
+}
+
+if (previewContainer) {
+  init();
+}
